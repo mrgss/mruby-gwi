66657dd64c789565881b588c95d43ed8109aac93
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2015-10-05 15:53:49 -0300

    Updating travis config again and again

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -2,4 +2,13 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem '../mruby-gwi'
+
+  conf.cc do |cc|
+    cc.command = "i686-w64-mingw32-gcc"
+  end
+
+  conf.cxx do |cxx|
+    cxx.command = "i686-w64-mingw32-g++"
+  end
+
 end
